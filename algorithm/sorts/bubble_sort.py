615939961071effc897126187a4e88c7b2d013d7
coder-oldgeek <you@coder-oldgeek>
2021-09-27 21:40:24 +0800

    0927 init

--- algorithm/sorts/bubble_sort.py
+++ algorithm/sorts/bubble_sort.py
@@ -1,14 +1,21 @@
-def bubble_sort(input_list: list[int]) -> list[int]:
+def bubble_sort(input_list: list) -> list:
     """
     冒泡排序
     href: https://baike.baidu.com/item/%E5%86%92%E6%B3%A1%E6%8E%92%E5%BA%8F/4602306?fr=aladdin
     input_list: list[int] 数值型数据列表
     returns: list[int]: 排序后数据列表
     """
-    n = n + 1
-    return ['2', '3']
+    size = len(input_list)
+    for i in range(size - 1):
+        for j in range(size-1, i, -1):
+            if input_list[j] < input_list[j-1]:
+                input_list[j], input_list[j-1] = input_list[j-1], input_list[j]
+    return input_list
 
 
 if __name__ == "__main__":
-    sorted()
-    pass
+    print(bubble_sort(["a"]))
+    print(bubble_sort([3, 2, 2]))
+    print(bubble_sort([0, 5, 2, 3, 2]))
+    print(bubble_sort([-23, 0, 6, -4, 34]))
+    print(bubble_sort(['d', 'a', 'b', 'e', 'c']))
--- algorithm/sorts/selection_sort.py
+++ algorithm/sorts/selection_sort.py
@@ -1,12 +1,24 @@
-def selection_sort(input_list: list[int]) -> list[int]:
+def selection_sort(input_list: list) -> list:
     """
     选择排序
     href: https://baike.baidu.com/item/%E9%80%89%E6%8B%A9%E6%8E%92%E5%BA%8F/9762418?fr=aladdin
     input_list: list[int] 数值型数据列表
     returns: list[int]: 排序后数据列表
     """
-    pass
+    num = 0
+    size = len(input_list)
+    for i in range(size):
+        for j in range(i+1, size):
+            if input_list[j] < input_list[num]:
+                num = j
+        input_list[i], input_list[num] = input_list[num], input_list[i]
+        num = i + 1
+    return input_list
 
 
 if __name__ == "__main__":
-    pass
+    print(selection_sort([]))
+    print(selection_sort(["a"]))
+    print(selection_sort([3, 2, 2]))
+    print(selection_sort([0, 5, 2, 3, 2]))
+    print(selection_sort([-23, 0, 6, -4, 34]))
