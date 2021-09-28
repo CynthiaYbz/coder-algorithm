5143c748727d97b8a819e26bdd4761ef6ea89628
coder-oldgeek <you@coder-oldgeek>
2021-09-28 19:21:47 +0800

    no message

--- algorithm/sorts/selection_sort.py
+++ algorithm/sorts/selection_sort.py
@@ -2,13 +2,13 @@ def selection_sort(input_list: list) -> list:
     """
     选择排序
     href: https://baike.baidu.com/item/%E9%80%89%E6%8B%A9%E6%8E%92%E5%BA%8F/9762418?fr=aladdin
-    input_list: list[int] 数值型数据列表
-    returns: list[int]: 排序后数据列表
+    input_list: list 数值型数据列表
+    returns: list: 排序后数据列表
     """
     num = 0
     size = len(input_list)
     for i in range(size):
-        for j in range(i+1, size):
+        for j in range(i + 1, size):
             if input_list[j] < input_list[num]:
                 num = j
         input_list[i], input_list[num] = input_list[num], input_list[i]
